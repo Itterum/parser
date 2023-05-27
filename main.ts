96247c3b1f8ba2238fe82e13d6e6a5882e923f66
itterum <itterum@proton.me>
2023-05-28 01:30:23 +0300

    refactor

--- main.ts
+++ main.ts
@@ -1,17 +1,7 @@
-import { chromium, Page } from 'playwright';
+import { chromium } from 'playwright';
 import { Database } from 'sqlite3';
 
-interface Price {
-    value: number;
-    currency: string;
-}
-
-interface Product {
-    name: string;
-    newPrice: Price;
-    oldPrice: Price;
-    image: string;
-}
+import { parsePage } from './scrap_functions/centrsvyazi';
 
 const createProductTable = `
     CREATE TABLE IF NOT EXISTS products (
@@ -22,54 +12,10 @@ const createProductTable = `
         oldPriceValue REAL,
         oldPriceCurrency TEXT,
         image TEXT
-    )
-`;
-
-async function parsePage(page: Page): Promise<Product[]> {
-    // Ожидаем появления списка товаров на странице
-    await page.waitForSelector('.products .product-item');
-
-    // Получаем список товаров
-    const products = await page.$$eval('.products .product-item', (elements) => {
-        return elements.map((element) => {
-            const nameElement = element.querySelector('.product_link > h3');
-            const newPriceElement = element.querySelector('.price_cart > .doubleprice > .newprice');
-            const oldPriceElement = element.querySelector('.price_cart > .doubleprice > .oldprice');
-            const imageElement = element.querySelector('.product-image');
-
-            const name = nameElement?.textContent?.trim() || '';
-
-            const newPriceText = newPriceElement?.textContent?.trim() || '';
-            const [newValue, newCurrency] = newPriceText.split(' ');
-            const newPrice = {
-                value: parseFloat(newValue.replace(',', '.')),
-                currency: newCurrency || '',
-            };
-
-            const oldPriceText = oldPriceElement?.textContent?.trim() || '';
-            const [oldValue, oldCurrency] = oldPriceText.split(' ');
-            const oldPrice = {
-                value: parseFloat(oldValue.replace(',', '.')),
-                currency: oldCurrency || '',
-            };
-
-            const image = `https://centrsvyazi.ru${imageElement?.getAttribute('style')?.match(/url\(['"]?(.*?)['"]?\)/)?.[1]}` || '';
-
-            return {
-                name,
-                newPrice,
-                oldPrice,
-                image,
-            };
-        });
-    });
-
-    return products;
-}
+    )`;
 
 async function run() {
     const db = new Database('./products.db');
-    let products;
 
     // Создание таблицы, если она не существует
     db.exec(createProductTable);
@@ -79,14 +25,25 @@ async function run() {
 
     await page.goto('https://centrsvyazi.ru/catalog/phones/apple');
 
-
-    products = await parsePage(page);
+    const products = await parsePage(page);
 
     // Сохранение каждого продукта в базу данных
     for (const product of products) {
-        db.run(
-            `INSERT INTO products (name, newPriceValue, newPriceCurrency, oldPriceValue, oldPriceCurrency, image) VALUES (?, ?, ?, ?, ?, ?)`,
-            [product.name, product.newPrice.value, product.newPrice.currency, product.oldPrice.value, product.oldPrice.currency, product.image],
+        db.run(`INSERT INTO products (
+                    name, 
+                    newPriceValue, 
+                    newPriceCurrency, 
+                    oldPriceValue, 
+                    oldPriceCurrency, 
+                    image) VALUES (?, ?, ?, ?, ?, ?)`,
+            [
+                product.name,
+                product.newPrice.value,
+                product.newPrice.currency,
+                product.oldPrice.value,
+                product.oldPrice.currency,
+                product.image
+            ],
             (err) => {
                 if (err) {
                     console.error('Ошибка при сохранении продукта в базе данных:', err);
---
+++ scrap_functions/centrsvyazi.ts
@@ -0,0 +1,55 @@
+import { Page } from 'playwright';
+
+interface Price {
+    value: number;
+    currency: string;
+}
+
+interface Product {
+    name: string;
+    newPrice: Price;
+    oldPrice: Price;
+    image: string;
+}
+
+export async function parsePage(page: Page): Promise<Product[]> {
+    // Ожидаем появления списка товаров на странице
+    await page.waitForSelector('.products .product-item');
+
+    // Получаем список товаров
+    const products = await page.$$eval('.products .product-item', (elements) => {
+        return elements.map((element) => {
+            const nameElement = element.querySelector('.product_link > h3');
+            const newPriceElement = element.querySelector('.price_cart > .doubleprice > .newprice');
+            const oldPriceElement = element.querySelector('.price_cart > .doubleprice > .oldprice');
+            const imageElement = element.querySelector('.product-image');
+
+            const name = nameElement?.textContent?.trim() || '';
+
+            const newPriceText = newPriceElement?.textContent?.trim() || '';
+            const [newValue, newCurrency] = newPriceText.split(' ');
+            const newPrice = {
+                value: parseFloat(newValue.replace(',', '.')),
+                currency: newCurrency || '',
+            };
+
+            const oldPriceText = oldPriceElement?.textContent?.trim() || '';
+            const [oldValue, oldCurrency] = oldPriceText.split(' ');
+            const oldPrice = {
+                value: parseFloat(oldValue.replace(',', '.')),
+                currency: oldCurrency || '',
+            };
+
+            const image = `https://centrsvyazi.ru${imageElement?.getAttribute('style')?.match(/url\(['"]?(.*?)['"]?\)/)?.[1]}` || '';
+
+            return {
+                name,
+                newPrice,
+                oldPrice,
+                image,
+            };
+        });
+    });
+
+    return products;
+}
